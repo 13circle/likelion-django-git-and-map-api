c31b81a5ed4cf91f8faf717dd5d0f0fad4f4a327
13circle <13circle97@gmail.com>
2020-07-28 16:33:19 +0900

    Create map API app with home template

--- djangoMapApiProject/djangoMapApiProject/urls.py
+++ djangoMapApiProject/djangoMapApiProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import mapapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mapapp.views.home, name = 'home'),
 ]
